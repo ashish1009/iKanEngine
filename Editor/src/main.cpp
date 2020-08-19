ae2f274c1235a223a4dbb1f74a2e86ad03dd10cd
ashish1009 <aashishkan1009@gmail.com>
2020-08-19 16:55:49 +0530

    Game Engine : Rendering Frist Quad with Element Buffer

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -81,9 +81,16 @@ int main(int argc, const char * argv[])
     
     float vertices[] =
     {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+    
+    unsigned int indices[] =
+    {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
     
     // Creating Vertex Array
@@ -96,6 +103,12 @@ int main(int argc, const char * argv[])
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // Creating Element Buffer
+    uint32_t EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // lInking vertex Attributes
     glEnableVertexAttribArray(0);
@@ -223,8 +236,8 @@ int main(int argc, const char * argv[])
         
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
