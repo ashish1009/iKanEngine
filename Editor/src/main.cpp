2fc1ded7cf91ca4731620fda94cd0443492ec45e
ashish1009 <aashishkan1009@gmail.com>
2020-08-19 19:27:04 +0530

    Game Engine : Adding Textures to our quad

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iKan.h>
 
+#include <stb_image.h>
+
 int main(int argc, const char * argv[])
 {
     iKan::Log::Init();
@@ -18,16 +20,16 @@ int main(int argc, const char * argv[])
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
     // Creating Window
-    int width = 1280, height = 720;
+    int windowWidth = 1280, windowHeight = 720;
     std::string  title = "iKan Engine";
-    GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, title.c_str(), nullptr, nullptr);
     if (!window)
     {
         IK_CORE_CRITICAL("Can not create wWindow");
         glfwTerminate();
         return -1;
     }
-    IK_CORE_INFO("Creating Window : {0} ({1}x{2})", title, width, height);
+    IK_CORE_INFO("Creating Window : {0} ({1}x{2})", title, windowWidth, windowHeight);
     
     // make GLFW Window Context
     glfwMakeContextCurrent(window);
@@ -47,7 +49,7 @@ int main(int argc, const char * argv[])
     IK_CORE_INFO("Version  : {0} ", glGetString(GL_VERSION));
     
     // set the view Port
-    glViewport(0, 0, width, height);
+    glViewport(0, 0, windowWidth, windowHeight);
     
     // GLFW Callbacks foe events
     glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height)
@@ -81,16 +83,17 @@ int main(int argc, const char * argv[])
     
     float vertices[] =
     {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
+        /* Position */          /* Color */                 /* TexCoord */
+         0.5f,  0.5f, 0.0f,     0.3f, 0.5f, 0.6f, 1.0f,     0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f,     0.3f, 0.5f, 0.6f, 1.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f,     0.3f, 0.5f, 0.6f, 1.0f,     1.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,     0.3f, 0.5f, 0.6f, 1.0f,     1.0f, 0.0f
     };
     
     unsigned int indices[] =
-    {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+    {
+        0, 1, 3,
+        1, 2, 3
     };
     
     // Creating Vertex Array
@@ -111,31 +114,50 @@ int main(int argc, const char * argv[])
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // lInking vertex Attributes
+    // Position
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0,
-                          3,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          3 * sizeof(float),
-                          (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
     
+    // Color
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)12);
+
+    // TexCoord
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)28);
+
     std::string vertexSrc = R"(
     #version 330 core
-    layout (location = 0) in vec3 aPos;
+    
+    layout (location = 0) in vec3 a_Position;
+    layout (location = 1) in vec4 a_Color;
+    layout (location = 2) in vec2 a_TexCoord;
+    
+    out vec4 v_Color;
+    out vec2 v_TexCoord;
     
     void main()
     {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    v_Color     = a_Color;
+    v_TexCoord  = a_TexCoord;
+    
+    gl_Position = vec4(a_Position.x, a_Position.y, a_Position.z, 1.0);
     }
     )";
     
     std::string fragmentSrc = R"(
     #version 330 core
+    
     out vec4 FragColor;
+    
+    in vec4 v_Color;
+    in vec2 v_TexCoord;
+    
+    uniform sampler2D u_Texture;
     
     void main()
     {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = texture(u_Texture, v_TexCoord) * v_Color;
     }
     )";
     
@@ -228,14 +250,70 @@ int main(int argc, const char * argv[])
     // Bind the shader
     glUseProgram(shaderProgram);
     
+    // Texture
+    stbi_set_flip_vertically_on_load(1);
+    
+    int height, width, channel;
+    
+    stbi_uc* data = nullptr;
+    data = stbi_load("../../Editor/assets/textures/Checkerboard.png", &width, &height, &channel, 0);
+    
+    IK_CORE_ASSERT(data, "Failed to load stbi Image");
+
+    GLenum internalFormat = GL_RGB8;
+    GLenum dataFormat     = GL_RGB;
+    
+    if (4 == channel)
+    {
+        internalFormat = GL_RGBA8;
+        dataFormat     = GL_RGBA;
+    }
+    else if (3 == channel)
+    {
+        internalFormat = GL_RGB8;
+        dataFormat     = GL_RGB;
+    }
+    IK_CORE_ASSERT((internalFormat & dataFormat), "invalid Format ");
+    
+    uint32_t textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
+    
+    if (data)
+        stbi_image_free(data);
+    
+    // Upload Texture to shader at slot 0
+    int location = glGetUniformLocation(shaderProgram, "u_Texture");
+    
+    if (-1 == location)
+        IK_CORE_WARN("Warning: uniform '{0}' doesnt exist!", "u_Texture");
+    
+    glUniform1i(location, 0);
+
     // Game Loop
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        // Bind Shader
         glUseProgram(shaderProgram);
+        
+        // Bind VertexArray
         glBindVertexArray(VAO);
+        
+        // Bind Texture
+        glActiveTexture(GL_TEXTURE0 + 0);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        
+        // Draw Element
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
