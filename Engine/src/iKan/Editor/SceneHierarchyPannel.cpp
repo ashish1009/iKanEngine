abfcc4985c9a128ba257b1867985759dd62ec733
ashish1009 <aashishkan1009@gmail.com>
2020-10-24 15:03:39 +0530

    Game Engine : Adding Sprite Renderer Component in scene Hierarchy Panne

--- Engine/src/iKan/Editor/SceneHierarchyPannel.cpp
+++ Engine/src/iKan/Editor/SceneHierarchyPannel.cpp
@@ -170,6 +170,16 @@ namespace iKan {
             }
             ImGui::Separator();
         } // if (entity.HasComponent<CameraComponent>())
+        
+        if (entity.HasComponent<SpriteRendererComponent>())
+        {
+            if (ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Sprite Renderer"))
+            {
+                auto& src = entity.GetComponent<SpriteRendererComponent>();
+                ImGui::ColorEdit4("Color", glm::value_ptr(src.Color));
+                ImGui::TreePop();
+            }
+        } // if (entity.HasComponent<SpriteRendererComponent>())
     }
     
 }
